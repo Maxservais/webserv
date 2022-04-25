b96fade0b7357a181bd02e95d512ce52dd2912b2
Marie Declerfayt <mdeclerf@fu-r7-p5.s19.be>
2022-04-25 17:34:56 +0200

    link

--- main.cpp
+++ main.cpp
@@ -7,27 +7,37 @@
 #include <fcntl.h>
 #include <vector>
 
+std::string read_file_to_str(const std::string& path)
+{
+	std::ifstream input_file(path);
+	if (!input_file.is_open()) 
+	{
+		std::cout << "Failed to open the requested ressource" << std::endl;
+	}
+	return std::string((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
+}
+
 std::string convert_to_binary(const char * path)
 {
 	std::vector<char> buffer;
-	FILE * file_stream = fopen(path, "rb");
-	int file_size;
-
-	if (file_stream != nullptr)
+	std::ifstream stream;
+	stream.open(path, std::ifstream::binary);
+	if (!stream.is_open())
 	{
-		fseek(file_stream, 0, SEEK_END); // https://www.javatpoint.com/fseek-in-c The fseek() function is used to set the file pointer to the specified offset. It is used to write data into file at desired location.
-		long file_length = ftell(file_stream); // ftell() in C is used to find out the position of file pointer in the file with respect to starting of the file.
-		rewind(file_stream); // The rewind() function sets the file position indicator for the stream pointed to by stream to the beginning of the file.
-
-		buffer.resize(file_length);
-
-		file_size = fread(&buffer[0], 1, file_length, file_stream);
+		std::cerr << path << " failed to open." << std::endl;
+		exit(EXIT_FAILURE);
 	}
+	stream.seekg(0, std::ios_base::end);
+	long file_length = stream.tellg();
+	stream.seekg(0);
+
+	buffer.resize(file_length);
+	stream.read(&buffer[0], file_length); // /!\ a proteger, je regarde ca
 
 	std::string file_str(buffer.begin(), buffer.end());
 	std::string file_size_str = std::to_string(file_str.length());
-	// std::string ret = "HTTP/1.1 200 OK\nContent-Type: image/png; \nContent-Length: " + file_size_str + file_str + "/n/n";
 	std::string ret = "HTTP/1.1 200 OK\r\nContent-Type: image/png; Content-Transfer-Encoding: binary; Content-Length: " + file_size_str + ";charset=ISO-8859-4 \r\n\r\n" + file_str;
+	stream.close();
 	return (ret);
 }
 
@@ -61,44 +71,77 @@ int	main(void)
 		return (EXIT_FAILURE);
 	}
 
-	/* Grab a connection from the queue */
-	socklen_t addrlen = sizeof(sockaddr);
-	int	connection = accept(sockfd, (struct sockaddr*)&sockaddr, &addrlen);
-	if (connection < 0)
-	{
-		std::cout << "Failed to grab connection!" << std::endl;
-		return (EXIT_FAILURE);
-	}
-
-	/* Read from the connection and print output */
 	char	buffer[1000000];
-	read(connection, buffer, 1000000);
-	std::cout << "Message was: " << buffer << std::endl;
-
-	/* Send a message to the connection */
-	std::string response = "HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\nContent-Length: 7650\n\n<!DOCTYPE html><html> <head><meta charset=\"utf-8\">server Response</head><body><h1> This page was render direcly from the server <p>Hello there welcome to my website</p></h1><img src=\"ball.png\"></body></html>";
-	send(connection, response.c_str(), response.size(), 0);
-
-	close(connection);
-	connection = accept(sockfd, (struct sockaddr*)&sockaddr, &addrlen);
-	if (connection < 0)
+	while(1)
 	{
-		std::cout << "Failed to grab connection!" << std::endl;
-		return (EXIT_FAILURE);
+		socklen_t addrlen = sizeof(sockaddr);
+
+		/* HEADERS */
+		int	connection = accept(sockfd, (struct sockaddr*)&sockaddr, &addrlen);
+		if (connection < 0)
+		{
+			std::cout << "Failed to grab connection!" << std::endl;
+			return (EXIT_FAILURE);
+		}
+
+		std::cout << buffer << std::endl;
+		memset(buffer, 0, 1000000);
+		read(connection, buffer, 1000000);
+		std::cout << buffer << std::endl;
+
+		std::string response = "HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\nContent-Length: 7650\n\n" + read_file_to_str("index.html");
+		send(connection, response.c_str(), response.size(), 0);
+
+		close(connection);
+
+		/* IMAGE */
+		connection = accept(sockfd, (struct sockaddr*)&sockaddr, &addrlen);
+		if (connection < 0)
+		{
+			std::cout << "Failed to grab connection!" << std::endl;
+			return (EXIT_FAILURE);
+		}
+
+		memset(buffer, 0, 1000000);
+		read(connection, buffer, 1000000);
+		std::cout << buffer << std::endl;
+
+		const char * path = "ball.png";
+		response = convert_to_binary(path);
+		send(connection, response.c_str(), response.size(), 0);
+
+		close(connection);
+
+		/* POST */
+		connection = accept(sockfd, (struct sockaddr*)&sockaddr, &addrlen);
+		if (connection < 0)
+		{
+			std::cout << "Failed to grab connection!" << std::endl;
+			return (EXIT_FAILURE);
+		}
+		memset(buffer, 0, 1000000);
+		read(connection, buffer, 1000000);
+		std::cout << buffer << std::endl;
+
+		/* LIEN */
+		connection = accept(sockfd, (struct sockaddr*)&sockaddr, &addrlen);
+		if (connection < 0)
+		{
+			std::cout << "Failed to grab connection!" << std::endl;
+			return (EXIT_FAILURE);
+		}
+
+		memset(buffer, 0, 1000000);
+		read(connection, buffer, 1000000);
+		std::cout << buffer << std::endl;
+		if ( buffer[5] == 'p' && buffer[6] == 'a' && buffer[7] == 'g')
+		{
+			response = "HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\nContent-Length: 7650\n\n" + read_file_to_str("page.html");
+			send(connection, response.c_str(), response.size(), 0);
+		}
+		close(connection);
 	}
 
-	char	buffer_2[1000000];
-	read(connection, buffer_2, 1000000);
-	std::cout << "Message was for second request: " << buffer_2 << std::endl;
-
-	const char * path = "ball.png";
-	response = convert_to_binary(path);
-	send(connection, response.c_str(), response.size(), 0);
-
-	/* Close the connections */
-	close(connection);
 	close(sockfd);
-	// close(fd);
-
 	return (EXIT_SUCCESS);
 }
