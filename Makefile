805a878c2541730163afd6b03e18407806f43a78
Marie Declerfayt <mdeclerf@fu-r7-p5.s19.be>
2022-04-26 14:00:14 +0200

    DELETE and POST, permissions and error codes to figure out

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CXX			=	c++
 
 CXXFLAGS	=	-Wall -Wextra -Werror -std=c++98
 
-SRC_FILES	=	test.cpp
+SRC_FILES	=	main.cpp
 
 OBJ_FILES	=	$(SRC_FILES:.cpp=.o)
 
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdlib.h>
@@ -6,6 +7,18 @@
 #include <fstream>
 #include <fcntl.h>
 #include <vector>
+#include <cstdio>
+
+#define SERVER_PORT 9999
+
+std::vector<std::string> split_file_name(std::string name)
+{
+	std::vector<std::string> splitted_file_name;
+	int pos = name.find_first_of('.');
+	splitted_file_name.push_back(name.substr(pos + 1));
+	splitted_file_name.push_back(name.substr(0,pos));
+	return splitted_file_name;
+}
 
 std::string read_file_to_str(const std::string& path)
 {
@@ -35,18 +48,15 @@ std::string convert_to_binary(const char * path)
 	stream.read(&buffer[0], file_length); // /!\ a proteger, je regarde ca
 
 	std::string file_str(buffer.begin(), buffer.end());
-	std::string file_size_str = std::to_string(file_str.length());
-	std::string ret = "HTTP/1.1 200 OK\r\nContent-Type: image/png; Content-Transfer-Encoding: binary; Content-Length: " + file_size_str + ";charset=ISO-8859-4 \r\n\r\n" + file_str;
 	stream.close();
-	return (ret);
+	return (file_str);
 }
 
-int	main(void)
+int main()
 {
 	int			sockfd;
 	sockaddr_in	sockaddr;
 
-	/* Create a socket (IPv4, TCP) */
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0)
 	{
@@ -54,17 +64,15 @@ int	main(void)
 		return (EXIT_FAILURE);
 	}
 
-	/* Assign an IP address and port to the socket */
 	sockaddr.sin_family = AF_INET;
 	sockaddr.sin_addr.s_addr = INADDR_ANY;
-	sockaddr.sin_port = htons(8080); // convert a number to the network byte order
+	sockaddr.sin_port = htons(SERVER_PORT); // convert a number to the network byte order
 	if (bind(sockfd, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) < 0)
 	{
 		std::cout << "Failed to bind !" << std::endl;
 		return (EXIT_FAILURE);
 	}
 
-	/* Start listening (max 10 connections in the queue) */
 	if (listen(sockfd, 10) < 0)
 	{
 		std::cout << "Failed to listen on socket!" << std::endl;
@@ -76,7 +84,6 @@ int	main(void)
 	{
 		socklen_t addrlen = sizeof(sockaddr);
 
-		/* HEADERS */
 		int	connection = accept(sockfd, (struct sockaddr*)&sockaddr, &addrlen);
 		if (connection < 0)
 		{
@@ -84,61 +91,69 @@ int	main(void)
 			return (EXIT_FAILURE);
 		}
 
-		std::cout << buffer << std::endl;
 		memset(buffer, 0, 1000000);
 		read(connection, buffer, 1000000);
 		std::cout << buffer << std::endl;
 
-		std::string response = "HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\nContent-Length: 7650\n\n" + read_file_to_str("index.html");
+		std::vector<std::string> vector;
+		std::string a;
+		std::string b;
+		int i = 0;
+		while(buffer[i] != ' ')
+			a.push_back(buffer[i++]);
+		i++;
+		while(buffer[i] != ' ')
+			b.push_back(buffer[i++]);
+		vector.push_back(a);
+		vector.push_back(b);
+
+		std::string response;
+		if (vector[0] == "GET")
+		{
+			if (vector[1] == "/index.html" || vector[1] == "/")
+				response = "HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\nContent-Length: 7650\n\n" + read_file_to_str("index.html");
+			else if (vector[1] == "/ball.png" || vector[1] == "/favicon.ico")
+			{
+				const char * path = "ball.png";
+				response = convert_to_binary(path);
+				std::string response_len = std::to_string(response.length());
+				std::string ret = "HTTP/1.1 200 OK\r\nContent-Type: image/png; Content-Transfer-Encoding: binary; Content-Length: " + response_len + ";charset=ISO-8859-4 \r\n\r\n" + response;
+			}
+			else if (vector[1] == "/page.html")
+				response = "HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\nContent-Length: 7650\n\n" + read_file_to_str("page.html");
+			else 
+				response = "HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\nContent-Length: 7650\n\n" + read_file_to_str("error404.html");
+			// send(connection, response.c_str(), response.size(), 0);
+		}
+
+		else if (vector[0] == "DELETE")
+		{
+			if ( vector[1] == "/ball.png" || vector[1] == "/page.html" || vector[1] == "/index.html")
+			{
+				vector[1].erase(0,1);
+				const char * file_name = const_cast<char*>(vector[1].c_str());
+				if (remove(file_name) != 0)
+					response = "HTTP/1.1 204 No Content\n\nContent-Type: text/plain\n";
+				else
+					response = "HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\nContent-Length: 48\n\n<html><body><h1>File deleted.</h1></body></html>";
+			}
+			else
+				response = "HTTP/1.1 204 No Content\n\nContent-Type: text/plain\n";
+		}
+
+		else if (vector[0] == "POST")
+		{
+			std::cout << "I AM A POST" << std::endl;
+			response = "HTTP/1.1 204 No Content\n\nContent-Type: text/plain\n";
+		}
+		else
+		{
+			std::cout << "NOT A POST OR GET" << std::endl;
+			response = "HTTP/1.1 204 No Content\n\nContent-Type: text/plain\n";
+		}
 		send(connection, response.c_str(), response.size(), 0);
-
 		close(connection);
-
-		/* IMAGE */
-		connection = accept(sockfd, (struct sockaddr*)&sockaddr, &addrlen);
-		if (connection < 0)
-		{
-			std::cout << "Failed to grab connection!" << std::endl;
-			return (EXIT_FAILURE);
-		}
-
-		memset(buffer, 0, 1000000);
-		read(connection, buffer, 1000000);
-		std::cout << buffer << std::endl;
-
-		const char * path = "ball.png";
-		response = convert_to_binary(path);
-		send(connection, response.c_str(), response.size(), 0);
-
-		close(connection);
-
-		/* POST */
-		connection = accept(sockfd, (struct sockaddr*)&sockaddr, &addrlen);
-		if (connection < 0)
-		{
-			std::cout << "Failed to grab connection!" << std::endl;
-			return (EXIT_FAILURE);
-		}
-		memset(buffer, 0, 1000000);
-		read(connection, buffer, 1000000);
-		std::cout << buffer << std::endl;
-
-		/* LIEN */
-		int connection_2 = accept(sockfd, (struct sockaddr*)&sockaddr, &addrlen);
-		if (connection_2 < 0)
-		{
-			std::cout << "Failed to grab connection_2!" << std::endl;
-			return (EXIT_FAILURE);
-		}
-
-		memset(buffer, 0, 1000000);
-		read(connection_2, buffer, 1000000);
-		std::cout << buffer << std::endl;
-		response = "HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\nContent-Length: 7650\n\n" + read_file_to_str("page.html");
-		send(connection_2, response.c_str(), response.size(), 0);
-		close(connection_2);
 	}
-
 	close(sockfd);
 	return (EXIT_SUCCESS);
 }
